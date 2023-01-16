91a0d94566d648f6fa4c3ae54debe83b26d8043e
diozenio <d.dionisiobsl@gmail.com>
2023-01-16 19:42:53 -0300

    UI - Add player controls component

---
+++ src/ui/components/Player/Controls/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./styles.css";
+
+export default function Controls() {
+    return (
+        <div className='controls-container'>
+            <span className="material-symbols-rounded">fast_rewind</span>
+            <span className="material-symbols-rounded">play_arrow</span>
+            <span className="material-symbols-rounded">fast_forward</span>
+        </div>
+    )
+}
