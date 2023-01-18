4926027e6e9da5dc6365dc4417f5aded04b28603
diozenio <d.dionisiobsl@gmail.com>
2023-01-18 17:56:17 -0300

    Music Player - Add song and author name from the context

--- src/ui/contexts/MusicCTX/MusicProvider.tsx
+++ src/ui/contexts/MusicCTX/MusicProvider.tsx
@@ -1,9 +1,30 @@
 import { MusicCTX } from ".";
-import { PropsWithChildren, useState, useCallback } from "react";
+import { PropsWithChildren, useState, useEffect } from "react";
 
 function MusicProvider({ children }: PropsWithChildren) {
     const [isPaused, setIsPaused] = useState<boolean>(true);
     const [currentTime, setCurrentTime] = useState<number>(0);
+    const [songName, setSongName] = useState<string>("");
+    const [author, setAuthor] = useState<string>("");
+    const [currentSong, setCurrentSong] = useState<HTMLAudioElement>(new Audio());
+    const [duration, setDuration] = useState<number>(0);
+
+
+    useEffect(() => {
+        const song = new Audio("audios/just_you_and_i.mp3");
+
+        song.onloadedmetadata = () => {
+            setCurrentSong(song);
+            setDuration(song.duration);
+        }
+
+        setSongName("Just You and I");
+        setAuthor("Tom Walker");
+        console.log("CTX UseEffect");
+    }, []);
+
+    const songLoaded = !!songName && !!author && !!currentSong && !!duration;
+
     function resume() {
         console.log("CTX Resume");
         currentSong.play();
--- src/ui/pages/Home/index.tsx
+++ src/ui/pages/Home/index.tsx
@@ -1,31 +1,40 @@
+import { useContext } from "react";
 import Player from "../../components/Player";
+import { MusicCTX } from "../../contexts/MusicCTX";
 import "./styles.css";
 
 function Home() {
-    return (
-        <div className="container">
-            <Player style={{ gridArea: "A" }} isVertical={true}>
-                <Player.AlbumCover imageURL="https://cdns-images.dzcdn.net/images/cover/f9a097c4dd46de803574f0450427dd5e/264x264.jpg"></Player.AlbumCover>
-                <Player.Song>Just you and I</Player.Song>
-                <Player.Author>Tom Walker</Player.Author>
-                <Player.Controls />
-                <Player.ProgressBar />
-            </Player>
-            <Player style={{ gridArea: "B" }}>
-                <Player.AlbumCover imageURL="https://cdns-images.dzcdn.net/images/cover/f9a097c4dd46de803574f0450427dd5e/264x264.jpg"></Player.AlbumCover>
-                <Player.Song>Just you and I</Player.Song>
-                <Player.Author>Tom Walker</Player.Author>
-                <Player.Controls />
-                <Player.ProgressBar />
-            </Player>
-            <Player style={{ gridArea: "C" }}>
-                <Player.AlbumCover imageURL="https://cdns-images.dzcdn.net/images/cover/f9a097c4dd46de803574f0450427dd5e/264x264.jpg"></Player.AlbumCover>
-                <Player.Song>Just you and I</Player.Song>
-                <Player.Author>Tom Walker</Player.Author>
-                <Player.Controls />
-            </Player>
-        </div>
+    const { author, songName, songLoaded } = useContext(MusicCTX);
 
+    if(songLoaded) {
+        return (
+            <div className="container">
+                <Player style={{ gridArea: "A" }} isVertical={true}>
+                    <Player.AlbumCover imageURL="https://cdns-images.dzcdn.net/images/cover/f9a097c4dd46de803574f0450427dd5e/264x264.jpg"></Player.AlbumCover>
+                    <Player.Song>{songName}</Player.Song>
+                    <Player.Author>{author}</Player.Author>
+                    <Player.Controls />
+                    <Player.ProgressBar />
+                </Player>
+                <Player style={{ gridArea: "B" }}>
+                    <Player.AlbumCover imageURL="https://cdns-images.dzcdn.net/images/cover/f9a097c4dd46de803574f0450427dd5e/264x264.jpg"></Player.AlbumCover>
+                    <Player.Song>{songName}</Player.Song>
+                    <Player.Author>{author}</Player.Author>
+                    <Player.Controls />
+                    <Player.ProgressBar />
+                </Player>
+                <Player style={{ gridArea: "C" }}>
+                    <Player.AlbumCover imageURL="https://cdns-images.dzcdn.net/images/cover/f9a097c4dd46de803574f0450427dd5e/264x264.jpg"></Player.AlbumCover>
+                    <Player.Song>{songName}</Player.Song>
+                    <Player.Author>{author}</Player.Author>
+                    <Player.Controls />
+                </Player>
+            </div>
+        )
+    }
+    
+    return (
+        <h1>Carregando</h1>
     )
 }
 
